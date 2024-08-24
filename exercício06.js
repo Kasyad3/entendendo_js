c0894fd0966c3146cc0106a6a784a97933e4433a
Kasyad3 <paulo.hprosa@senacsp.edu.br>
2024-08-23 21:46:38 -0300

    feat: Exercício 6 finalizado!

--- exercício06.js
+++ exercício06.js
@@ -1,3 +1,9 @@
-Nome =(prompt("Digite seu nome: "))
-GanhoPorHora =parseFloat(prompt("Quanto ganha por hora? "))
-HorasTrabalhadasMes =parseFloat(prompt("Quantas horas trabalha por mês? "))
+let Nome = (prompt("Digite seu nome: "));
+let GanhoPorHora = parseFloat(prompt("Quanto ganha por hora? "));
+let HorasTrabalhadasMes = parseFloat(prompt("Quantas horas trabalha por mês? "));
+
+let TotalRenda = GanhoPorHora * HorasTrabalhadasMes
+
+let mensagem = (Nome + " Recebe " + TotalRenda + " De renda total ao mês trabalhado. ");
+
+alert(mensagem)
