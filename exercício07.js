7ce21859c2b871a78440c1dcedc4372996af3664
Kasyad3 <paulo.hprosa@senacsp.edu.br>
2024-08-27 19:36:44 -0300

    feat: Exercício 7 finalizado.

---
+++ exercício07.js
@@ -0,0 +1,15 @@
+let altura = parseFloat(prompt("Qual sua altura? "));
+let idade = parseFloat(prompt("Qual sua idade? "));
+
+if (altura >= 1.70) {
+    mensagem ="Acesso permitido"}
+
+else if (altura >= 1.60 && idade >= 18) {
+    mensagem = "Acesso permitido"}
+
+else
+{mensagem = "Acesso negado"};
+
+alert(mensagem)
+
+
